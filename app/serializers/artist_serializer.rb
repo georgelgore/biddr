93c66674f5fbce84bee6b2557ab75edd7615ae41
George Gore <georgelgore@gmail.com>
2018-02-02 09:44:29 -0500

    reseed and update morning push

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,7 +1,11 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :name, :title_name
+  attributes :name, :title_name, :image
 
   def title_name
     object.name.titlecase
+  end
+
+  def image
+    object.lots.last.image
   end
 end
