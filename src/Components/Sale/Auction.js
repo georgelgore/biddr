daaa06f32d60ab1e6e660b2c04424c14edc60416
George Gore <georgelgore@gmail.com>
2018-03-05 12:47:49 -0500

    rever auction route change

--- src/Components/Sale/Auction.js
+++ src/Components/Sale/Auction.js
@@ -15,24 +15,24 @@ const Auction = props => {
           <tbody className="center aligned">
             <tr>
               <td>
-                <Link to="biddr/auctions/2007">2007</Link>
+                <Link to="/auctions/2007">2007</Link>
               </td>
               <td>
-                <Link to="biddr/auctions/2011">2011</Link>
+                <Link to="/auctions/2011">2011</Link>
               </td>
               <td>
-                <Link to="biddr/auctions/2015">2015</Link>
+                <Link to="/auctions/2015">2015</Link>
               </td>
             </tr>
             <tr>
               <td>
-                <Link to="biddr/auctions/2008">2008</Link>
+                <Link to="/auctions/2008">2008</Link>
               </td>
               <td>
-                <Link to="biddr/auctions/2012">2012</Link>
+                <Link to="/auctions/2012">2012</Link>
               </td>
               <td>
-                <Link to="biddr/auctions/2016">2016</Link>
+                <Link to="/auctions/2016">2016</Link>
               </td>
             </tr>
             <tr>
@@ -44,15 +44,15 @@ const Auction = props => {
               </td>
 
               <td>
-                <Link to="biddr/auctions/2017">2017</Link>
+                <Link to="/auctions/2017">2017</Link>
               </td>
             </tr>
             <tr>
               <td>
-                <Link to="biddr/auctions/2010">2010</Link>
+                <Link to="/auctions/2010">2010</Link>
               </td>
               <td>
-                <Link to="biddr/auctions/2014">2014</Link>
+                <Link to="/auctions/2014">2014</Link>
               </td>
               <td />
             </tr>
