97f043580b3afe63e8a668de031b688dfafb6cf9
George Gore <georgegore@Georges-MBP.verizon.net>
2018-03-12 14:52:36 -0400

    update artist route

---
+++ app/controllers/api/v1/artist_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::ArtistssController < ApplicationController
+  def index
+    @artists = Artist.all
+    render json: ArtistListSerializer.new(@artists).serialized_json
+  end
+
+  def show
+    @sale = Artist.find(params[:id])
+    render json: ArtistSerializer.new(@artist).serialized_json
+  end
+
+end
